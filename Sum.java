7a5313479546011a6bc39af5e84a0dc260faff9d
Bhavuk Sharma <bhavuk55555@gmail.com>
2023-05-28 20:24:28 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,12 @@
+class Sum{
+   public void add(int a, int b){
+    SYstem.out.println("Sum is= "+(a+b));
+   }
+
+   public static void main(String[] args){
+
+    Sum s= new Sum();
+    s.add(2,4);
+   }
+
+}
