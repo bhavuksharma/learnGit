8a30dcd6efa4874617dbcc04e7b594fd93ef4feb
bhavuk sharma <bhavuk55555@gmail.com>
2025-01-09 12:24:50 +0530

    added method

--- prcm/Multiply.java
+++ prcm/Multiply.java
@@ -1,5 +1,10 @@
 public class Multiply {
     public static void main(String[] args){
-        System.out.println("multiplication is= "+(5*4));
+        System.out.println("multiplication is= "+multiply(5,4));
+    }
+
+    public static int multiply(int a , int b){
+
+        return a * b;
     }
 }
